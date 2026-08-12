613bf1b9c24c7cf107ac652b7133e9e88750d39c
Krish_Kanjani <krishkanjani86@gmail.com>
2026-08-12 20:07:26 +0530

    Added a basic k6 script in /loadG/scripts and made a dedicated k6 metrics dashboard load-t-o.json

---
+++ harness/load-generation/scripts/cpu-hog.js
@@ -0,0 +1,18 @@
+import http from 'k6/http';
+import {check,sleep} from 'k6';
+
+export const options={
+    stages:[
+        {duration:'10s',target:5}, //ramp up to 5 users
+        {duration:'20s',target:15}, //spike to 15 users
+        {duration:'10s',target:0}, //ramp down
+    ],
+}
+
+export default function(){
+    const res=http.get('http://host.docker.internal:3001/fibonacci?n=35');
+    check(res,{
+        'status is 200': (r)=>r.status=200
+    });
+    sleep(0.5); //small pause between request per virtual user 
+}
